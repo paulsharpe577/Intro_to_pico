c0c364867511319e9e1ec0bfd2a53a4e166ba95d
Paul Sharpe <44348094+paulsharpe577@users.noreply.github.com>
2024-09-06 09:58:31 +0100

    Update main.py

--- 01_servos/servoupdown/main.py
+++ 01_servos/servoupdown/main.py
@@ -1,1 +1,16 @@
+#Imports
+from servo import Servo
+import time
 
+#Pin setup
+sg90_servo = Servo(pin=28)  
+
+while True:
+        for x in range(180, 0, -1):
+            sg90_servo.move(x)
+            print(x)
+            time.sleep(0.01)
+        for x in range (0, 180):
+            sg90_servo.move(x)
+            print(x)
+            time.sleep(0.01)
